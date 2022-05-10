5f6a3bec3469aaac40d5e845267bfacafcba799f
gauhar <2464231008@qq.com>
2022-05-10 21:04:02 +0800

    feat: init

---
+++ README.md
@@ -0,0 +1,5 @@
+[在线 excel 转 JSON](https://uutool.cn/excel2json/)
+
+1. 先把 excel 表里面的内容全选复制，打开上面的网址，粘贴进去。然后复制结果
+2. 打开`dataCI.js` ，把等号后面的东西全部删除。然后粘贴刚刚复制的结果；右键格式化文档，ctrl+s 保存
+3. 在终端运行`node cicd.js`
---
+++ cicd.js
@@ -0,0 +1,61 @@
+const fs = require("fs");
+const data = require("./dataCI");
+const child_process = require("child_process");
+const json2xls = require("json2xls");
+
+function getContent(item) {
+  return `variables:
+  GIT_STRATEGY: clone
+
+stages:
+  - deploy
+
+deploy:
+  stage: deploy
+  tags:
+    - ${item.runner}
+  script:
+    - npm install --unsafe-perm=true --allow-root
+    - rm -rf ./dist
+    - >${item.测试服务器路径 ? `\n      if [ "$CI_BUILD_REF_NAME" == "test" ]; then
+        echo 'test branch'
+        ${item.测试环境自定义命令 || 'npm run build:test'}
+        scp -r ./dist/* ${item.测试服务器路径}${item.开发服务器路径 ? '' : '\n      fi'}` : ''}${item.开发服务器路径 ? `${item.测试服务器路径 ? '\n      elif' : '\n      if'} [ "$CI_BUILD_REF_NAME" == "dev" ]; then
+        echo 'dev branch'
+        ${item.开发环境自定义命令 || 'npm run build:dev'}
+        scp -r ./dist/* ${item.开发服务器路径}
+      fi` : ''}${item.机器人地址 ? `\n    - curl ${item.机器人地址}` : ''}
+  except:
+    - master
+`
+}
+
+
+const newDirName = '生成好的配置文件';
+const configList = []
+// 删除文件夹
+child_process.exec(`rm -rf ./${newDirName}`, () => {
+  fs.mkdir(`./${newDirName}`, () => {
+    const groups = [... new Set(data.map(item => item.群组名))]
+    // 生成文件夹
+    groups.forEach(item => fs.mkdirSync(`./${newDirName}/${item}`))
+    
+    data.forEach(item => {
+      const ymlPath = `./${newDirName}/${item.群组名}/.${item.项目名称}-gitlab-ci.yml`
+      const ymlName = `${item.群组名}/.${item.项目名称}-gitlab-ci.yml`
+      fs.writeFile(ymlPath, getContent(item), 'utf-8', (error) => {
+        if (error) console.log(error)
+      })
+      configList.push({
+        '项目名称': item.项目名称,
+        '配置文件地址': `http://192.168.8.244/honedu/sre/config/-/raw/main/frontend/${ymlName}`
+      })
+    })
+    toExcel(configList)
+  });
+});
+
+function toExcel(data) {
+  const xls = json2xls(data);
+  fs.writeFileSync(`./CICD配置网址.xlsx`, xls, "binary");
+}
